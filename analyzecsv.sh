047fba3f914ad923c00dbdef38143aca75093295
AaronNoletPDIT <aaron@pdit.co>
2020-04-24 21:15:03 -0400

    2

--- analyzecsv.sh
+++ analyzecsv.sh
@@ -3,7 +3,7 @@ OLDIFS=$IFS
 IFS=","
 
 HEADVALS=$( head -n 1 $1 )
-HEADMOD=$( sed -i 's/ /_/g' )
+HEADMOD=$( sed -i 's/ /_/g' $HEADVALS)
 
 echo -e "Header Vals from $1:\n-------------------\n$HEADVALS\n\n\n$HEADMOD"
 
