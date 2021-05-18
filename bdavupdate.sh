050a563847c069246af9751dbbf389204d895ab8
AaronNolet <40274249+AaronNolet@users.noreply.github.com>
2021-05-18 16:10:14 -0400

    BitDefender update script test

---
+++ bdavupdate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+# Install a trap to catch errors and clean-up temp files
+trap 'echo "Installer terminated. Exit.";' INT TERM EXIT
+# rm -f -r ./.tmp/'
+if (( $EUID != 0 )); then
+    echo "$(tput setaf 1)Please run as root$(tput sgr 0)"
+    exit
+fi
+wget http://download.bitdefender.com/updates/bitdefender_v9/cumulative.zip
+unzip cumulative.zip -o -d /images/dev/rescue/lib/scan/Plugins
+chown root:root /images/dev/rescue/lib/scan/Plugins -R
